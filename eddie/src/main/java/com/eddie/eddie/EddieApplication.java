881aed3a4f2a3b17a43587b9915bd066f6d9e480
Talon Marquard <tmarquard@10xpeople.com>
2021-03-31 19:56:49 -0600

    initial commit for Eddie

---
+++ eddie/src/main/java/com/eddie/eddie/EddieApplication.java
@@ -0,0 +1,13 @@
+package com.eddie.eddie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EddieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EddieApplication.class, args);
+	}
+
+}
---
+++ eddie/src/test/java/com/eddie/eddie/EddieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eddie.eddie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EddieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
